e87313aac0aa43c9adfcea58782c8cc7b7013630
sqwei <m18610844053@163.com>
2023-03-30 15:13:51 +0800

    update

---
+++ upload_to_cos.sh
@@ -0,0 +1,4 @@
+coscmd upload my3.0shuyuan.json 3.0/
+coscmd upload my2.0shuyuan.json 2.0/
+echo "2.0: https://yuedushuyuan-1251974059.cos.ap-beijing.myqcloud.com/2.0/my2.0shuyuan.json"
+echo "3.0: https://yuedushuyuan-1251974059.cos.ap-beijing.myqcloud.com/3.0/my3.0shuyuan.json"
