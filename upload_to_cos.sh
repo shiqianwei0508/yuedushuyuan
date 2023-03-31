eb196f9aaf59e4c2d2787c441b2b15eaa1b57766
sqwei <m18610844053@163.com>
2023-03-31 10:41:13 +0800

    update

--- upload_to_cos.sh
+++ upload_to_cos.sh
@@ -1,6 +1,4 @@
-coscmd upload my3.0shuyuan.json 3.0/
-coscmd upload my2.0shuyuan.json 2.0/
+coscmd upload my$1shuyuan.json $1/
 echo
 echo
-echo "2.0: https://yuedushuyuan-1251974059.cos.ap-beijing.myqcloud.com/2.0/my2.0shuyuan.json"
-echo "3.0: https://yuedushuyuan-1251974059.cos.ap-beijing.myqcloud.com/3.0/my3.0shuyuan.json"
+echo "$1: https://yuedushuyuan-1251974059.cos.ap-beijing.myqcloud.com/$1/my$1shuyuan.json"
